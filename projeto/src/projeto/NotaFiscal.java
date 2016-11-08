8b64a6e32e1a78a5011e15bfe326b04bb74ba150
gerlaneln <g2fjac@gmail.com>
2016-11-08 19:24:41 -0200

    Enviando classes

---
+++ projeto/src/projeto/NotaFiscal.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class NotaFiscal {
+    private int numero;
+    private String data;
+    private int cnpj;
+    
+    public void getValorNota(){
+    }
+    
+    public void getfaturamento(){
+    }
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the data
+     */
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the cnpj
+     */
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    /**
+     * @param cnpj the cnpj to set
+     */
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+}
--- projeto/src/projeto/Projeto.java
+++ projeto/src/projeto/Projeto.java
@@ -15,6 +15,8 @@ public class Projeto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("COISA");
         // TODO code application logic here
     }
     
